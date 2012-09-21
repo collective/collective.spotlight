09e545e71360460714a42f2920d2f79d137d7de7
claytonc <claytonc.sousa@gmail.com>
2012-09-21 14:43:12 -0300

    corrections in configuration

--- setup.py
+++ setup.py
@@ -2,37 +2,47 @@ from setuptools import setup, find_packages
 import os
 
 version = '1.0a1'
+long_description = open("README.txt").read() + "\n" + \
+                   open(os.path.join("docs", "INSTALL.txt")).read() + "\n" + \
+                   open(os.path.join("docs", "CREDITS.txt")).read() + "\n" + \
+                   open(os.path.join("docs", "HISTORY.txt")).read()
 
 setup(name='collective.spotlight',
       version=version,
       description="",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=long_description,
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
       classifiers=[
         "Framework :: Plone",
+        "Framework :: Plone :: 4.2",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Topic :: Software Development :: Libraries :: Python Modules"
         ],
-      keywords='"Framework :: Plone",',
+      keywords='plone dexterity spotlight',
       author='Clayton Caetano de Sousa',
       author_email='claytonc.sousa@gmail.com',
-      url='"Programming Language :: Python",',
-      license='"Topic :: Software Development :: Libraries :: Python Modules",',
-      packages=find_packages(exclude=['ez_setup']),
+      url='https://github.com/collective/collective.spotlight',
+      license='GPLv2',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
       namespace_packages=['collective'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
           'setuptools',
+          'Plone>=4.2',
           # -*- Extra requirements: -*-
       ],
+      extras_require={
+        'test': ['plone.app.testing'],
+        },
       entry_points="""
       # -*- Entry points: -*-
 
       [z3c.autoinclude.plugin]
       target = plone
       """,
-      setup_requires=["PasteScript"],
-      paster_plugins=["ZopeSkel"],
       )
