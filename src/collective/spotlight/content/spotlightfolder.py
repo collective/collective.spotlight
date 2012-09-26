61ae169b6ad7fbc62911de04da80ead1187a3cc0
claytonc <claytonc.sousa@gmail.com>
2012-09-26 10:51:43 -0300

    building types with dexterity

--- src/collective/spotlight/content/spotlightfolder.py
+++ src/collective/spotlight/content/spotlightfolder.py
@@ -4,10 +4,10 @@ from plone.directives import form
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 from collective.spotlight import _
 
-styles = SimpleVocabulary(
-    [SimpleTerm(value=u'0', title=_(u'Display with image and title')),
-     SimpleTerm(value=u'1', title=_(u'Display with title')),
-     SimpleTerm(value=u'2', title=_(u'Display with image and title and description'))]
+styles_portlet = SimpleVocabulary(
+    [SimpleTerm(value=u'0', title=_(u'image and title')),
+     SimpleTerm(value=u'1', title=_(u'title')),
+     SimpleTerm(value=u'2', title=_(u'image and title and description'))]
     )
 
 
@@ -15,9 +15,9 @@ class ISpotlightFolder(form.Schema):
     """A container for spotlight
     """
 
-    display = schema.Choice(
-            title=_(u"Display the box"),
+    display_portlet = schema.Choice(
+            title=_(u"Display the portlet"),
             description=_(u"Choose which display box fits the content that will be inserted."),
-            vocabulary=styles,
+            vocabulary=styles_portlet,
             required=True,
     )
